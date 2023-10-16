153dd477dab61a01237595d3eb7414b52e2ca2a6
MaraKa.exe <94853428+MarcASO1560@users.noreply.github.com>
2023-10-16 04:12:08 +0200

    Pequeño comentario.

--- app/src/main/java/com/example/momentoisaac/MainActivity.kt
+++ app/src/main/java/com/example/momentoisaac/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.momentoisaac
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
@@ -13,18 +12,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //Segun si se ha pulsado el botón, cambiará permanentemente la imagen o no.
+
         val buttonPrincipal: Button = findViewById(R.id.principal)
         val imageView: ImageView = findViewById(R.id.imagen_principal)
-        buttonPrincipal.setOnClickListener(View.OnClickListener {
-            // Verificar el estado de la imagen y cambiarla en función de eso
-            var imagenCambiada = true
+        buttonPrincipal.setOnClickListener{
+            val imagenCambiada = true
             if (imagenCambiada) {
                 imageView.setImageResource(R.mipmap.isaac_maricon_feliz)
-                imagenCambiada = true
             } else {
                 imageView.setImageResource(R.mipmap.isaac_maricon)
-                imagenCambiada = false
             }
-        })
+        }
     }
 }
