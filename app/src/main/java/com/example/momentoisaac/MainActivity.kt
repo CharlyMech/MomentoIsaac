7fa72e19407642e34fe80a710369288ddcbe3dc3
MaraKa.exe <94853428+MarcASO1560@users.noreply.github.com>
2023-10-16 04:50:23 +0200

    Pantalla de inicio principal terminada.

--- app/src/main/java/com/example/momentoisaac/MainActivity.kt
+++ app/src/main/java/com/example/momentoisaac/MainActivity.kt
@@ -4,6 +4,9 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -12,14 +15,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //Segun si se ha pulsado el botón, cambiará permanentemente la imagen o no.
-
         val buttonPrincipal: Button = findViewById(R.id.principal)
         val imageView: ImageView = findViewById(R.id.imagen_principal)
-        buttonPrincipal.setOnClickListener{
-            val imagenCambiada = true
-            if (imagenCambiada) {
+
+        buttonPrincipal.setOnClickListener {
+            // Cambiar la imagen.
+            val hahaha = true
+            if (hahaha) {
                 imageView.setImageResource(R.mipmap.isaac_maricon_feliz)
+
+                // Iniciar una corutina para esperar 2 segundos antes de cambiar al otro layout.
+                GlobalScope.launch {
+                    delay(1000)
+                    runOnUiThread {
+                        setContentView(R.layout.activity_menu)
+                    }
+                }
             } else {
                 imageView.setImageResource(R.mipmap.isaac_maricon)
             }
