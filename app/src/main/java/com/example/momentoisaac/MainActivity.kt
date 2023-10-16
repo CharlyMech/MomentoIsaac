c4c19f450b82e0453abc96649a7acb3c41c0612f
MaraKa.exe <94853428+MarcASO1560@users.noreply.github.com>
2023-10-16 04:05:23 +0200

    Muchas cosas...

--- app/src/main/java/com/example/momentoisaac/MainActivity.kt
+++ app/src/main/java/com/example/momentoisaac/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.momentoisaac
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonPrincipal: Button = findViewById(R.id.principal)
+        val imageView: ImageView = findViewById(R.id.imagen_principal)
+        buttonPrincipal.setOnClickListener(View.OnClickListener {
+            // Verificar el estado de la imagen y cambiarla en función de eso
+            var imagenCambiada = true
+            if (imagenCambiada) {
+                imageView.setImageResource(R.mipmap.isaac_maricon_feliz)
+                imagenCambiada = true
+            } else {
+                imageView.setImageResource(R.mipmap.isaac_maricon)
+                imagenCambiada = false
+            }
+        })
     }
 }
